1acc1743b3e07d8bc3f0727407eec15ff47cf5b3
Putra Prima Arhandi <putraprima@gmail.com>
2020-10-01 04:37:13 +0700

    tambah library dan start portal.js

---
+++ portal.js
@@ -0,0 +1,38 @@
+const puppeteer = require("puppeteer");
+const BASE_URL = "https://portal.polinema.ac.id";
+const moment = require("moment");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+const portal = {
+  browser: null,
+  page: null,
+  maxDelay: 5000,
+  minDelay: 3000,
+  init: async () => {
+    try {
+      console.log("Browser Start");
+      portal.browser = await puppeteer.launch({
+        product: "firefox",
+        headless: false,
+        defaultViewport: null,
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          // "--start-fullscreen",
+        ],
+      });
+
+      portal.page = await travian.browser.newPage();
+
+      await portal.page.goto(BASE_URL, { waitUntil: "networkidle2" });
+    } catch (error) {
+      //kalo error langsung close browser
+      console.log(error.stack);
+      await portal.browser.close();
+    }
+  },
+};
+module.exports = portal;
