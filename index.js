e9ea73aec5cf54b996c55f04a6ca466a5480f920
annisataufika <annisa.tfirdausi@gmail.com>
2020-10-17 06:16:21 +0700

    Update index.js
    Komentar Looping User

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
+
 (async () => {
   //   await portal.init();
+  // looping user
   let users = await db.get("users").value();
   for (let user of users) {
     console.log(user);
