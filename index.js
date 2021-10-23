a2b4bb10f15350f8fb21ba147af8afb55cd685dd
Putra Prima Arhandi <putraprima@gmail.com>
2021-10-23 07:45:04 +0700

    Add Comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const path = require("path");
+//load dotenv
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
+//load portal
 const portal = require("./portal");
 const db = require("./db.json");
 
@@ -8,6 +10,7 @@ const db = require("./db.json");
   const { users } = db;
   await portal.shuffle(users);
   await portal.init();
+  //iterate for each users
   for (const user of users) {
     await portal.login(user.username, user.password);
     await portal.clickDataKehadiran();
